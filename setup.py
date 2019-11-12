6ff8d5dbb7fbb24fb16f75f4af6d1698f8ff6ebe
duythien0912 <duythien0912@gmail.com>
2019-11-12 15:19:40 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@
 
 #from pip.req import parse_requirements
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+# try: # for pip >= 10
+#     from pip._internal.req import parse_requirements
+# except ImportError: # for pip <= 9.0.3
+#     from pip.req import parse_requirements
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 try:
     from setuptools import setup
